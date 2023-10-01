3c7fc61f87f5a394f5b7f06b58fafb61076c44af
Testernysa <138409852+Testernysa@users.noreply.github.com>
2023-10-01 23:19:57 +0200

    Add files via upload

---
+++ task-02.js
@@ -0,0 +1,26 @@
+
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const ingredientsList = document.getElementById("ingredients");
+
+const createIngredientsList = (ingredients) => {
+  const fragment = document.createDocumentFragment();
+
+  ingredients.forEach(ingredient => {
+    const listItem = document.createElement("li");
+    listItem.textContent = ingredient;
+    listItem.classList.add("item");
+    fragment.appendChild(listItem);
+  });
+
+  ingredientsList.appendChild(fragment);
+};
+
+createIngredientsList(ingredients);
